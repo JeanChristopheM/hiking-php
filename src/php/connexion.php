103365168198090a8311c9fbeb280dfe91b3fd77
Francis <francisfrancois9189@gmail.com>
2021-12-03 15:47:07 +0100

    setting up gitignore

--- src/php/connexion.php
+++ src/php/connexion.php
@@ -1,9 +1,9 @@
 <?php
-    define('HOST', 'mysql');
-    define('USR', 'root');
-    define('PWD', 'root');
+    define('HOST', '188.166.24.55');
+    define('USR', 'jepsen5-billygoat');
+    define('PWD', '$PMGgeWFMo#lW0>r;Rg?');
     try {
-        $db = new PDO('mysql:host='.HOST.';dbname=hiking;port=3306', USR, PWD);
+        $db = new PDO('mysql:host='.HOST.';dbname='.USR.';port=3306', USR, PWD);
         $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     } catch(Exception $e) {
         echo $e->getMessage();
