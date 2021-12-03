75c473db0751b02b6e80f26e9f7694f969c6414b
Francis <francisfrancois9189@gmail.com>
2021-12-03 16:30:23 +0100

    update dataHikes

--- src/php/dataHikes.php
+++ src/php/dataHikes.php
@@ -1,13 +1,7 @@
 <?php
-echo ($_POST["name"]);
-echo ($_POST["difficulty"]);
-echo ($_POST["distance"]);
-echo ($_POST["duration"]);
-echo ($_POST["elevation"]);
 
 $name = $_POST["name"];
-filter_var($name, FILTER_SANITIZE_STRING);
-var_dump($name);
+$dataName = filter_var($name, FILTER_SANITIZE_STRING,FILTER_FLAG_STRIP_BACKTICK);
 
 
 $difficulty = $_POST["difficulty"];
@@ -17,15 +11,24 @@ $max = 2;
 if (filter_var($difficulty, FILTER_VALIDATE_INT, array("options" => array("min_range"=>$min, "max_range"=>$max))) === false) {
     echo("Number value is not within the legal range");
 } else {
-    echo("Number value is within the legal range");
+    $dataDifficulty = filter_var($difficulty, FILTER_VALIDATE_INT, array("options" => array("min_range"=>$min, "max_range"=>$max)));
 }
 
-
 $distance = $_POST["distance"];
-floatval(filter_var($distance, FILTER_SANITIZE_NUMBER_FLOAT,
+$dataDistance = floatval(filter_var($distance, FILTER_SANITIZE_NUMBER_FLOAT,
 FILTER_FLAG_ALLOW_FRACTION));
 
+$duration = $_POST["duration"];
+$dataDuration = filter_var($name, FILTER_SANITIZE_STRING,FILTER_FLAG_STRIP_BACKTICK);
 
+$elevation = $_POST["elevation"];
+$min = 0;
+$max = 2000;
 
+if (filter_var($difficulty, FILTER_VALIDATE_INT, array("options" => array("min_range"=>$min, "max_range"=>$max))) === false) {
+    echo("Number value is not within the legal range");
+} else {
+    $dataElevation = filter_var($difficulty, FILTER_VALIDATE_INT, array("options" => array("min_range"=>$min, "max_range"=>$max)));
+}
 ?>
 
