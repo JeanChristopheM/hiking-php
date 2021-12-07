53923ab1aaef75c640a2724e38c973a62c54dd93
Jean-Christophe Molhant <jean.christophe.molhant@gmail.com>
2021-12-07 16:54:27 +0100

    added more style and  added a modal to confirm deletion

--- src/index.php
+++ src/index.php
@@ -20,13 +20,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Hike manager. Browse hikes in Belgium and add your contribution.">
     <title>Hike-app</title>
     <link rel="stylesheet" href="/style/main.css">
 </head>
 <body>
     <header class="header">
-        <h1>Hike-App</h1>
-        <a href="./php/create.php" class="add">add new hike</a>
+        <h1 class="mainTitle">Hike-App 🥾</h1>
+        <a href="./php/create.php" class="add abutton">add new hike</a>
     </header>
     <main class="app">
         <?php
@@ -50,13 +51,26 @@
                         <p class="card__label">Elevation +</p>
                         <p class="card__data">'.$hike['elevation'].'m</p>
                     </div>
-                    <p class="delete"><a href=php/delete.php?ID='.$hike['ID'].'>DELETE</a></p>
-                    <p class="modify"><a href=php/update.php?ID='.$hike['ID'].'>MODIFY</a></p>
+                    <p class="delete"><a class="abutton delete" id='.$hike['ID'].'>DELETE</a></p>
+                    <p class="modify"><a class="abutton" href=php/update.php?ID='.$hike['ID'].' id='.$hike['ID'].'>MODIFY</a></p>
                 </div>
                 ';
             }
         ?>
     </main>
-
+    <?php include './php/includes/modal.php'; ?>
+    <script>
+        window.addEventListener('click', (e) => {
+            const modal = document.querySelector('.modal');
+            if(e.target.classList.contains('delete') && e.target.classList.contains('abutton')) {
+                modal.classList.remove('hidden');
+                const yes = document.querySelector('#modalYes');
+                yes.href = `php/delete.php?ID=${e.target.id}`;
+            }
+            if(e.target.id == "modalNo") {
+                modal.classList.add('hidden');
+            }
+        });
+    </script>
 </body>
 </html>
