c22d8fd5aa1621a9757277bb845b667fff400f26
Jean-Christophe Molhant <jean.christophe.molhant@gmail.com>
2021-12-08 16:44:28 +0100

    end of day debrief

--- src/index.php
+++ src/index.php
@@ -30,11 +30,11 @@ session_start();
         
         <h1 class="mainTitle">Hike-App 🥾</h1>
     </header>
-    <main class="app">
-        <?php
+    <?php
             if(!isset($_SESSION['user'])) {
                 include './php/includes/loginForm.php';
             } else {
+                echo '<main class="app">';
                 foreach ($hikes as $hike) {
                     $message='';
                     if($hike['createdAt'] === $hike['updatedAt']) {
@@ -43,37 +43,37 @@ session_start();
                         $message = 'Updated the '.$formatDate($hike['updatedAt']).'';
                     }
                     echo '
-                    <div class="card">
-                        <p class="name" style="font-weight:bold;">'.$hike['name'].'</p>
-                        <div class="difficulty">
-                            <p class="card__label">Difficulty</p>
-                            <p class="card__data">'.$difficulties[$hike['difficulty']].'</p>
+                        <div class="card">
+                            <p class="name" style="font-weight:bold;">'.$hike['name'].'</p>
+                            <div class="difficulty">
+                                <p class="card__label">Difficulty</p>
+                                <p class="card__data">'.$difficulties[$hike['difficulty']].'</p>
+                            </div>
+                            <div class="distance">
+                                <p class="card__label">Distance</p>
+                                <p class="card__data">'.$hike['distance'].'km</p>
+                            </div>
+                            <div class="duration">
+                                <p class="card__label">Duration</p>
+                                <p class="card__data">'.$hike['duration'].'</p>
+                            </div>
+                            <div class="elevation">
+                                <p class="card__label">Elevation +</p>
+                                <p class="card__data">'.$hike['elevation'].'m</p>
+                            </div>
+                            <div class="created">
+                                <p>'.$message.'</p>
+                            </div>
+                            <p class="delete"><a class="abutton delete" id='.$hike['ID'].'>DELETE</a></p>
+                            <p class="modify"><a class="abutton" href=php/update.php?ID='.$hike['ID'].' id='.$hike['ID'].'>MODIFY</a></p>
                         </div>
-                        <div class="distance">
-                            <p class="card__label">Distance</p>
-                            <p class="card__data">'.$hike['distance'].'km</p>
-                        </div>
-                        <div class="duration">
-                            <p class="card__label">Duration</p>
-                            <p class="card__data">'.$hike['duration'].'</p>
-                        </div>
-                        <div class="elevation">
-                            <p class="card__label">Elevation +</p>
-                            <p class="card__data">'.$hike['elevation'].'m</p>
-                        </div>
-                        <div class="created">
-                            <p>'.$message.'</p>
-                        </div>
-                        <p class="delete"><a class="abutton delete" id='.$hike['ID'].'>DELETE</a></p>
-                        <p class="modify"><a class="abutton" href=php/update.php?ID='.$hike['ID'].' id='.$hike['ID'].'>MODIFY</a></p>
-                    </div>
                     ';
                 }
+                echo '</main>';
             };
         
         
         ?>
-    </main>
     <?php 
         if(isset($_GET['message']) && !empty($_GET['message'])) {
             include './php/includes/message.php';
--- src/php/includes/nav.php
+++ src/php/includes/nav.php
@@ -8,8 +8,9 @@
     <?php else: ?>
         <div>
             <li><a href="/index.php">Home</a></li>
+            <li><a href="/php/profile.php">Profile</a></li>
             <li><a href="/php/logout.php">Logout</a></li>
         </div>
-        <li><a href="/php/create.php" class="add buttonhikes">Add New Hike</a></li>
+        <li><a href="/php/create.php" class="buttonhikes">Add New Hike</a></li>
     <?php endif; ?>
 </nav>
