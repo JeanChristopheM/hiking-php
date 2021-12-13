8475db332feb832c09561aac8acccf5439c5bce6
Jean-Christophe Molhant <jean.christophe.molhant@gmail.com>
2021-12-13 14:20:30 +0100

    added a subscribe button below the login on the index

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php 
 session_start();
+date_default_timezone_set('Europe/Brussels');
+
 ?>
 <?php
     require_once('./php/connexion.php');
@@ -17,54 +19,48 @@ session_start();
 <body>
     <?php include './php/includes/nav.php'; ?>
     <header class="header">
-        
         <h1 class="mainTitle">Hike-App 🥾</h1>
     </header>
-    
-        <?php
-            if(!isset($_SESSION['user'])) {
-                include './php/includes/loginForm.php';
-            } else {
-                echo '<main class="app">';
-                foreach ($hikes as $hike) {
-                    $message='';
-                    if($hike['createdAt'] === $hike['updatedAt']) {
-                        $message = 'Created the '.$formatDate($hike['createdAt']).'';
-                    } else {
-                        $message = 'Updated the '.$formatDate($hike['updatedAt']).'';
-                    }
-                    echo '
-                    <div class="card">
-                        <p class="name" style="font-weight:bold;">'.$hike['name'].'</p>
-                        <div class="difficulty">
-                            <p class="card__label">Difficulty</p>
-                            <p class="card__data">'.$difficulties[$hike['difficulty']].'</p>
-                        </div>
-                        <div class="distance">
-                            <p class="card__label">Distance</p>
-                            <p class="card__data">'.$hike['distance'].'km</p>
-                        </div>
-                        <div class="duration">
-                            <p class="card__label">Duration</p>
-                            <p class="card__data">'.$hike['duration'].'</p>
-                        </div>
-                        <div class="elevation">
-                            <p class="card__label">Elevation +</p>
-                            <p class="card__data">'.$hike['elevation'].'m</p>
-                        </div>
-                        <div class="created">
-                            <p>'.$message.'</p>
-                        </div>
-                        <p class="delete"><a class="abutton delete" id='.$hike['ID'].'>DELETE</a></p>
-                        <p class="modify"><a class="abutton" href=php/update.php?ID='.$hike['ID'].' id='.$hike['ID'].'>MODIFY</a></p>
-                    </div>
-                    ';
+    <?php if(!isset($_SESSION['user'])) : ?>
+        <?php include './php/includes/loginForm.php'; ?>
+        <p class="subscribeIndex">Not subscribed yet ? <a href="../php/subscription.php" class="abutton">SUBSCRIBE !</a></p>
+    <?php else: ?>
+        <main class="app">
+            <?php foreach ($hikes as $hike) : 
+                $message='';
+                if($hike['createdAt'] === $hike['updatedAt']) {
+                    $message = 'Created the '.$formatDate($hike['createdAt']).'';
+                } else {
+                    $message = 'Updated the '.$formatDate($hike['updatedAt']).'';
                 }
-                echo '</main>';
-            };
-        
-        
-        ?>
+            ?>
+            <div class="card">
+                <p class="name" style="font-weight:bold;"><?=$hike['name'];?></p>
+                <div class="difficulty">
+                    <p class="card__label">Difficulty</p>
+                    <p class="card__data"><?=$difficulties[$hike['difficulty']];?></p>
+                </div>
+                <div class="distance">
+                    <p class="card__label">Distance</p>
+                    <p class="card__data"><?=$hike['distance'];?>km</p>
+                </div>
+                <div class="duration">
+                    <p class="card__label">Duration</p>
+                    <p class="card__data"><?=$hike['duration'];?></p>
+                </div>
+                <div class="elevation">
+                    <p class="card__label">Elevation +</p>
+                    <p class="card__data"><?=$hike['elevation'];?>m</p>
+                </div>
+                <div class="created">
+                    <p><?=$message;?></p>
+                </div>
+                <p class="delete"><a class="abutton delete" id='<?=$hike['ID'];?>'>DELETE</a></p>
+                <p class="modify"><a class="abutton" href="php/update.php?ID=<?=$hike['ID'];?>" id="<?=$hike['ID'];?>">MODIFY</a></p>
+            </div>
+            <?php endforeach; ?>
+        </main>
+    <?php endif; ?>
     
     <?php 
         if(isset($_GET['message']) && !empty($_GET['message'])) {
